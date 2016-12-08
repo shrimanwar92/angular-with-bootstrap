a6582dbef0f0ca20cfa6ae3904df082b19ae6b8d
Nilay Shrimanwar <shrimanwar92@gmail.com>
2016-12-08 09:12:36 +0530

    added edit process dialog

--- src/app/components/process.component.ts
+++ src/app/components/process.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, EventEmitter } from '@angular/core';
 import { ProcessService } from './../services/process.service';
 import { SharedService } from './../services/shared.service';
 import {NgbModal, NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import * as settings from './../settings';
 
 @Component({
   selector: 'process-view',
@@ -18,10 +19,12 @@ export class ProcessComponent implements OnInit {
   	mode = 'Observable';
     pp: string = "";
     changeSub: any;
-    title: string = "Process List";
-    modalTitle: string = "Add Process";
-    dataAddedSuccessfully: boolean = false;
+    title: string = settings.PROCESS_LIST;
+    modalTitle: string = settings.ADD_PROCESS;
+    showSuccess: boolean = false;
+    successMessage: string = settings.PROCESS_ADD_SUCCESS;
     modalRef: any;
+    process: string;
 
 	constructor(private processService: ProcessService, private sharedService: SharedService, private modalService: NgbModal, activeModal: NgbActiveModal) { 
     this.pp = this.sharedService.getItemsPerPage();
@@ -45,12 +48,19 @@ export class ProcessComponent implements OnInit {
       )
   }
 
-  open(content) {
+  open(content, args) {
     /*this.modalService.open(content).result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
     }, (reason) => {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
     });*/
+    if(!args.add) {
+      this.modalTitle = settings.EDIT_PROCESS;
+      this.process = "asda";
+    } else {
+      this.modalTitle = settings.ADD_PROCESS;
+      this.process = "";
+    }
     this.modalRef = this.modalService.open(content);
   }
 
@@ -65,7 +75,12 @@ export class ProcessComponent implements OnInit {
   onSubmit(form: any, content): void {  
     console.log('you submitted value:', form);
     this.close();
-    this.dataAddedSuccessfully = true;
+    this.showSuccess = true;
+
+    setTimeout(() => {
+      this.showSuccess = false;
+    }, 10000);
+
   }
 
 }
--- src/app/settings.ts
+++ src/app/settings.ts
@@ -1,2 +1,6 @@
 
 export const PAGE_SIZES = ['15', '25', '50', '100'];
+export const EDIT_PROCESS = "Edit Process";
+export const ADD_PROCESS = "Add Process";
+export const PROCESS_ADD_SUCCESS = "Your process was saved successfully.";
+export const PROCESS_LIST = "Process List";
